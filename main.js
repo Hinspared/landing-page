8119bcc73f29de54757807cdac6f87fa5256543c
Marat Khasanov <Hinspared@MacBook-Air-Marat.local>
2022-05-19 20:59:26 +0200

    finish html&css

---
+++ main.js
@@ -0,0 +1,123 @@
+const allSections = document.querySelectorAll('.section');
+const cardsAboutEl = document.querySelector('.cards-about');
+const cardNumberEl = document.querySelectorAll('.card-number');
+const about = document.querySelector('.about');
+const chboxEl = document.querySelectorAll('.chbox');
+const labelEl = document.querySelectorAll('.label');
+const btnTransferEl = document.querySelector('.btn-transfer');
+const contactsEl = document.querySelector('.newtransaction-contacts');
+const headerEl = document.querySelector('.header');
+
+// Count animation from start to end
+const animateValue = (obj, start, end, duration) => {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min(
+      (timestamp - startTimestamp) / (duration * 1000),
+      1
+    );
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+};
+// Revealing section by intersecting
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('hidden');
+  // Reveal section ABOUT and animate count in cards
+  if (entry.target.classList.contains('home')) {
+    btn.classList.add('hidden');
+  } else {
+    btn.classList.remove('hidden');
+  }
+  if (entry.target.classList.contains('about')) {
+    cardNumberEl.forEach((card) => {
+      if (card.classList.contains('clients')) animateValue(card, 0, 50, 5);
+      if (card.classList.contains('projects')) animateValue(card, 0, 140, 5);
+      if (card.classList.contains('members')) animateValue(card, 0, 20, 5);
+      if (card.classList.contains('awards')) animateValue(card, 0, 30, 5);
+    });
+  }
+  console.log(entry);
+  // observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.05,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add('hidden');
+});
+
+const revealCheck = (el) => {
+  console.log(vectorEl);
+  return vectorEl.forEach((el) => el.classList.toggle('hidden'));
+};
+
+chboxEl.forEach((el) =>
+  el.addEventListener('click', function (e) {
+    const vectorEl = e.target
+      .closest('.chbox-container')
+      .querySelector('.vector-check');
+    if (!vectorEl) return;
+    return vectorEl.classList.toggle('hidden');
+  })
+);
+
+// Choose active user
+let activeUser;
+contactsEl.addEventListener('click', function (e) {
+  if (activeUser) activeUser.classList.remove('active');
+  const el = e.target.closest('.contact-transaction');
+  if (!el) return;
+  activeUser = el.querySelector('.contact-transaction--name');
+  console.log(el);
+  activeUser.classList.toggle('active');
+});
+
+// Do transfer feature
+const makeTransfer = () => {
+  if (!activeUser) {
+    alert('Choose account where money should be transfered');
+    return;
+  }
+  const sum = document.querySelector('.new-transaction--sum');
+  const money = (value, locale, currency) =>
+    new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currency,
+    }).format(value);
+  // Make transaction
+  alert(
+    `${money(sum.value, navigator.language, 'EUR')} was transfered to ${
+      activeUser.textContent
+    }`
+  );
+  // Clear value
+  sum.value = '';
+  activeUser.classList.remove('active');
+  activeUser = '';
+};
+btnTransferEl.addEventListener('click', makeTransfer);
+// Button to top
+let btn = document.createElement('button');
+btn.innerHTML = 'TO TOP';
+document.body.appendChild(btn);
+btn.classList.add('fixed', 'hidden');
+btn.addEventListener('click', function () {
+  headerEl.scrollIntoView({ behavior: 'smooth' });
+});
+// Slider
+
+// Change plans monthly/yearly
+// Add subscribe function
